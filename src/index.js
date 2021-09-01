48dcb21c7285d753e3c4c2890b97aac955379547
Roman Korotaev <kro1986@ukr.net>
2021-09-01 23:17:36 +0300

    Применен шаблон
    Работает с шаблонизатором  "parcel-plugin-handlebars-precompile": "^1.0.2"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import './sass/main.scss';
 import axios from 'axios';
+import onePhotoCardTpl from './templates/onePhotoCardTpl.hbs';
 
 console.log('Домашнее задание №13 по JS: goit-js-hw-13-image-finder');
 
 
-/*
-https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=23193680-1d95b6a7ab6e160162f942df5
-
-*/
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -17,53 +14,30 @@ const refs = {
 
 let pageCounter = 1;
 
- // Пример запроса на бекенд через функцию  библиотеку axios: npm install axios
+ // Пример запроса на бекенд через  библиотеку axios: npm install axios
  // сначала установить ее через коменду npm install, а потом экспортируем в файл index.js: import axios from 'axios';
 const hndlerSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault()  // сброс настроек по умолчанию
     const value = refs.input.value;
     console.log(value)
 
 axios.get(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${pageCounter}&per_page=12&key=23193680-1d95b6a7ab6e160162f942df5 `)
-    //.then(result => console.log(result.data.hits))
-    .then(result => renderCollection (result.data.hits))
-   // .then(result => renderCollection (result.data.drinks))
-      .catch(err => console.log(err))   
+    .then(result => { renderCollection(result.data.hits); console.log ("result.data.hits = ", result.data.hits) })
+    .catch(err => console.log(err))   
     
 }
 
-
-function createItem({webformatURL, tags, likes, views, comments, downloads}) {  
-    const onePhotoCard = `<li>
-       <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" />
-
-  <div class="stats">
-    <p class="stats-item">
-      <i class="material-icons">thumb_up (likes)</i>
-      ${likes}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">visibility (views)</i>
-      ${views}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">comment</i>
-      ${comments}
-    </p>
-    <p class="stats-item">
-      <i class="material-icons">cloud_download</i>
-      ${downloads}
-    </p>
-  </div>
-</div>
-        </li>
-    `
-    refs.gallery.insertAdjacentHTML('beforeend', onePhotoCard)
+// Функция получает объект, применяем распыление, чтобы вытянуть из него только те свойства, что нам нужны
+function createItem({ webformatURL, tags, likes, views, comments, downloads }) {
+    
+//Рендерим разментку из шаблона onePhotoCardTpl.hbs через функцию onePhotoCardTpl () . Был установлен пакет с шаблонизатором  "parcel-plugin-handlebars-precompile": "^1.0.2"
+refs.gallery.insertAdjacentHTML('beforeend', onePhotoCardTpl({webformatURL, tags, likes, views, comments, downloads}))
 }
 
 function renderCollection(arr) {
-    arr.forEach (el => createItem (el) )
+     //Чтобы нарисовать все карточки за одно обращение - после цикла map() применяем функцию массива join('')/
+    //и всю разметку всех элементов полученного массива превращаем в отду строку
+   return arr.map(createItem).join('');
 }
 
 
