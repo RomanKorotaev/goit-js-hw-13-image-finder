628c00367c21b483542ad5d9eed99258ef2c9000
Roman Korotaev <kro1986@ukr.net>
2021-09-04 12:37:23 +0300

    Добавлена проверка данных инпута

--- src/index2.js
+++ src/index2.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 import onePhotoCardTpl from './templates/onePhotoCardTpl.hbs';
 
 
+import { alert, info, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
 
 const imageApiService = new ImageApiService();
 
@@ -16,28 +19,53 @@ function onSearch(e) {
     e.preventDefault();
 
     //clearPhotosGallary();
-    imageApiService.query = e.currentTarget.elements.query.value;
-    imageApiService.resetPage();
-    // imageApiService.fetchImages().then(hits => console.log(hits));
-    imageApiService.fetchImages().then(hits=>{
-        clearPhotosGallary();
-        onePhotoCardMarkUp(hits);
-    });
+  if (e.currentTarget.elements.query.value === '') {alert({text: 'You didn`t enter a query!'});
+  } else {
+        imageApiService.query = e.currentTarget.elements.query.value;
+        imageApiService.resetPage();
+        imageApiService.fetchImages()
+          .then(hits => {
+            if (hits.length === 0) {
+              info({ text: 'No such images!' });
+            }
+            else {
+              clearPhotosGallary();
+              PhotoCardMarkUp(hits);
+              refs.loadMore.classList.remove("visually-hidden")
+            }         
+          })
+          .catch(() => { alert({ text: 'Ошибка при запросе на бекенд!'}); } );
+      
+      }
 }
- 
+
+
+// //  --------- Начало функции onSearch c более чистым синтаксисом, без проверки что ввёл пользователь -------//
+//  function onSearch(e) {
+//     e.preventDefault();
+//     imageApiService.query = e.currentTarget.elements.query.value;
+//     imageApiService.resetPage();
+//     imageApiService.fetchImages().then(hits=>{
+//         clearPhotosGallary();
+//         onePhotoCardMarkUp(hits);
+//     });
+// }
+// //  --------- Конец функции onSearch c более чистым синтаксисом, без проверки что ввёл пользователь -------//
+
 
 function onLoadMore() {
   //  imageApiService.fetchImages().then(hits => console.log(hits));
     imageApiService.fetchImages()
-        .then(onePhotoCardMarkUp)
+        .then(PhotoCardMarkUp)
         .then(() => {
       scrollGallery();
     });
+ 
 }
 
  
 // Функция получает объект, применяем распыление, чтобы вытянуть из него только те свойства, что нам нужны
-function onePhotoCardMarkUp(hits) {
+function PhotoCardMarkUp(hits) {
     
 //Рендерим разментку из шаблона onePhotoCardTpl.hbs через функцию onePhotoCardTpl () . Был установлен пакет с шаблонизатором  "parcel-plugin-handlebars-precompile": "^1.0.2"
 refs.gallery.insertAdjacentHTML('beforeend', onePhotoCardTpl(hits))
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,7 @@
 const refs = {
     searchForm: document.querySelector('#search-form'),
     input: document.querySelector('#input'),
-   gallery: document.querySelector('.gallery'),
+    gallery: document.querySelector('.gallery'),
     loadMore: document.querySelector('[data-action="load-more"]')
     //loadMore: document.querySelector('#loadMore')
 
